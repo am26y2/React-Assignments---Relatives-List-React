55c4707152a8701ffb9541c73b4c9424da74866d
Amit Raj <amitraj9046@gmail.com>
2020-11-09 19:50:39 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          <li key="relativeListItem1">Amit</li>
+          <li key="relativeListItem2">Ankit</li>
+          <li key="relativeListItem3">Rajesh</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
